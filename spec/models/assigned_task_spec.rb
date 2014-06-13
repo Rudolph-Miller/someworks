77decf6e43d313cc111a0b3c5b6eafa208b35db7
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2014-06-13 20:45:02 +0900

    specを書いた！

--- spec/models/assigned_task_spec.rb
+++ spec/models/assigned_task_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe AssignedTask do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it '正常に登録できること' do
+		assigned_task = FactoryGirl.build(:assigned_task)
+		expect {assigned_task.save!}.to_not raise_error
+	end
 end
