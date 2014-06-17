1dab0a9978e041d78155c2aed4b4c7efe7676a24
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2014-06-17 09:16:04 +0900

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140615155855) do
+ActiveRecord::Schema.define(version: 20140616230704) do
 
   create_table "articles", force: true do |t|
     t.string   "content"
@@ -88,6 +88,19 @@ ActiveRecord::Schema.define(version: 20140615155855) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "writers", ["email"], name: "index_writers_on_email", unique: true, using: :btree
+  add_index "writers", ["reset_password_token"], name: "index_writers_on_reset_password_token", unique: true, using: :btree
 
 end
