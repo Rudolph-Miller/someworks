00791fad5f7fe9393667d2840fe2466191f14f5e
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2014-06-19 21:26:56 +0900

    認証後のrouting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Publisher)
+      publishers_tasks_path
+    elsif resource_or_scope.is_a?(Editor)
+      editors_tasks_path
+    elsif resource_or_scope.is_a?(Writer)
+      writers_articles_path
+    elsif
+      super
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+      new_devise_session_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,50 +2,51 @@ Rails.application.routes.draw do
   devise_for :publishers
   devise_for :editors
   devise_for :writers
-  namespace :publishers do
-  get 'tasks', to: 'tasks#index'
-  post 'tasks/create', to: 'tasks#create'
-  get 'tasks/new'
-	get 'tasks/show', to: 'tasks#show'
-	patch 'tasks/update', to: 'tasks#update'
-	get 'tasks/edit', to: 'tasks#edit'
-	post 'tasks/delete', to: 'tasks#delete'
-	post 'tasks/publish', to: 'tasks#publish'
-	post 'tasks/unpublish', to: 'tasks#unpublish'
 
-  get 'websites', to: 'websites#index'
+  namespace :publishers do
+    get 'tasks', to: 'tasks#index'
+    post 'tasks/create', to: 'tasks#create'
+    get 'tasks/new'
+    get 'tasks/show', to: 'tasks#show'
+    patch 'tasks/update', to: 'tasks#update'
+    get 'tasks/edit', to: 'tasks#edit'
+    post 'tasks/delete', to: 'tasks#delete'
+    post 'tasks/publish', to: 'tasks#publish'
+    post 'tasks/unpublish', to: 'tasks#unpublish'
+
+    get 'websites', to: 'websites#index'
   end
 
   namespace :writers do
-  get 'articles', to: 'articles#index'
-  get 'articles/new', to: 'articles#new'
-	post 'articles/create', to: 'articles#create'
-	patch 'articles/create', to: 'articles#create'
-	get 'articles/show', to: 'articles#show'
-	patch 'articles/update', to: 'articles#update'
-	get 'articles/edit'
-	post 'articles/update'
-	post 'articles/delete', to: 'articles#delete'
-	post 'articles/upload_picture', to: 'articles#upload_picture'
-	match 'articles/store/images/:token1/:token2', to: 'articles#show_image', via: :get
+    get 'articles', to: 'articles#index'
+    get 'articles/new', to: 'articles#new'
+    post 'articles/create', to: 'articles#create'
+    patch 'articles/create', to: 'articles#create'
+    get 'articles/show', to: 'articles#show'
+    patch 'articles/update', to: 'articles#update'
+    get 'articles/edit'
+    post 'articles/update'
+    post 'articles/delete', to: 'articles#delete'
+    post 'articles/upload_picture', to: 'articles#upload_picture'
+    match 'articles/store/images/:token1/:token2', to: 'articles#show_image', via: :get
 
-  get 'assigned_tasks', to: 'assigned_tasks#index'
-	get 'assigned_tasks/show', to: 'assigned_tasks#show'
+    get 'assigned_tasks', to: 'assigned_tasks#index'
+    get 'assigned_tasks/show', to: 'assigned_tasks#show'
   end
 
   namespace :editors do
-  get 'tasks', to: 'tasks#index'
-	get 'tasks/show', to: 'tasks#show'
-	post 'tasks/assign', to: 'tasks#assign'
-	post 'tasks/unassign', :controller => 'tasks', :action => 'unassign'
-	get 'tasks/new', to: 'tasks#new'
-	post 'create', to: 'tasks#create'
-	post 'tasks/delete', :controller => 'tasks', :action => 'delete'
-	get 'tasks/edit', to: 'tasks#edit'
-	patch 'tasks/update', to: 'tasks#update'
+    get 'tasks', to: 'tasks#index'
+    get 'tasks/show', to: 'tasks#show'
+    post 'tasks/assign', to: 'tasks#assign'
+    post 'tasks/unassign', :controller => 'tasks', :action => 'unassign'
+    get 'tasks/new', to: 'tasks#new'
+    post 'create', to: 'tasks#create'
+    post 'tasks/delete', :controller => 'tasks', :action => 'delete'
+    get 'tasks/edit', to: 'tasks#edit'
+    patch 'tasks/update', to: 'tasks#update'
 
-  get 'assigned_tasks', to: 'assigned_tasks#index'
-	post 'assigned_tasks/unassign', :controller => 'assigned_tasks', :action => 'unassign'
+    get 'assigned_tasks', to: 'assigned_tasks#index'
+    post 'assigned_tasks/unassign', :controller => 'assigned_tasks', :action => 'unassign'
   end
 
 
