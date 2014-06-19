de0641aeb4c193cc61cc07d3f3fde695a59b90e1
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2014-06-19 11:58:12 +0900

    some fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,11 @@
 class Article < ActiveRecord::Base
-	default_scope {where(:deleted_at => nil)}
+  default_scope {where(:deleted_at => nil)}
 
-	belongs_to :writer
-	belongs_to :assigned_task
-	has_many :pictures
+  belongs_to :writer
+  belongs_to :assigned_task
+  has_many :pictures
 
-	def assigned_task_info ()
-		self.assigned_task.task
-	end
+  def assigned_task_info ()
+    self.assigned_task.task
+  end
 end
